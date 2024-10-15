0327f2a55918299426c8a354d178eb5b18296dee
inamoto <inamoto_yuta@cyberagent.co.jp>
2024-10-15 13:29:06 +0900

    fix: 時間の比較処理の修正とentryにない場合も通知を飛ばす

--- fetch_arxiv_papers.py
+++ fetch_arxiv_papers.py
@@ -20,7 +20,7 @@ time_threshold = now - timedelta(hours=5)
 
 # 検索クエリを定義
 # Computer Scienceのカテゴリを指定
-query = 'cat:cs.*'
+query = 'cat:cs.SD'
 
 # ベースとなるAPIのURL
 base_url = 'http://export.arxiv.org/api/query?'
@@ -29,7 +29,7 @@ base_url = 'http://export.arxiv.org/api/query?'
 params = {
     'search_query': query,
     'start': 0,                   # 取得開始位置
-    'max_results': 300,           # 取得する結果の最大数（必要に応じて増やしてください）
+    'max_results': 100,           # 取得する結果の最大数（必要に応じて増やしてください）
     'sortBy': 'submittedDate',    # 提出日の新しい順にソート
     'sortOrder': 'descending',
 }
@@ -43,16 +43,40 @@ url = base_url + query_string
 # フィードをパース
 feed = feedparser.parse(url)
 
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+    except ValueError as e:
+        print(f"Error parsing date: {e}")
+        return None
+
 # Discordに送信した論文の数をカウント
 paper_count = 0
+
+# フィードにエントリーが含まれていない場合
+if 'entries' not in feed:
+    # Discordに送信するペイロードを作成
+    payload = {
+        'content': "No entries found."
+    }
+
+    # DiscordのWebhookにPOSTリクエストを送信
+    response = requests.post(DISCORD_WEBHOOK_URL, data=payload)
+    print('No entries found.')
+    exit(0)
 
 # 各論文について、3時間以内に公開されたものをフィルタリングして情報を表示
 for entry in feed.entries:
     # 'published' フィールドの日付を解析
     published_str = entry.published
-    published = datetime.strptime(published_str, '%Y-%m-%dT%H:%M:%SZ')
+    published = parse_date(published_str)
+
+    if not published:
+        continue
     
-    # 'published' が3時間以内かを確認
+    print(published)
+    
+    # 'published' がtime_thresholdよりも新しい場合のみ処理を続行
     if published >= time_threshold:
         title = entry.title
         summary = entry.summary.replace('\n', ' ')  # 改行を削除して整形
@@ -74,6 +98,8 @@ for entry in feed.entries:
         payload = {
             'content': message_content
         }
+        
+        print(message_content)
 
         # DiscordのWebhookにPOSTリクエストを送信
         response = requests.post(DISCORD_WEBHOOK_URL, data=payload)
@@ -83,7 +109,7 @@ for entry in feed.entries:
         else:
             print(f'Sent paper ID {paper_id} to Discord.')
             paper_count += 1
-        time.sleep(3)  # 連続して送信しないように3秒待機
+        time.sleep(5)  # 連続して送信しないように3秒待機
 
 # 処理が完了したことを表示
 print(f'Total {paper_count} papers sent to Discord.')
