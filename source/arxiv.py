8cfcbe7441caf074bc2114739c434b8c865c30c8
inamoto <inamoto_yuta@cyberagent.co.jp>
2024-10-18 19:21:28 +0900

    style: arXivからのデータ取得を別クラスに切り分け

---
+++ source/arxiv.py
@@ -0,0 +1,23 @@
+import feedparser
+import requests
+
+MAX_RETRIES = 3
+RETRY_WAIT_TIME = 5  # 秒
+
+# フィードを取得する関数を定義
+def fetch_feed(url, retries=MAX_RETRIES) -> feedparser.FeedParserDict:
+    for attempt in range(retries):
+        try:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            }
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # HTTPエラーが発生した場合に例外を発生させる
+            return feedparser.parse(response.content)
+        except (requests.exceptions.RequestException, ConnectionResetError) as e:
+            print(f"Attempt {attempt + 1} failed: {e}")
+            if attempt < retries - 1:
+                time.sleep(RETRY_WAIT_TIME)
+            else:
+                print("Failed to fetch feed after multiple attempts.")
+                raise  # 最終試行でも失敗した場合は例外を再発生させる
--- source/fetch_arxiv_papers.py
+++ source/fetch_arxiv_papers.py
@@ -1,33 +1,12 @@
 import urllib.parse
-import feedparser
 from datetime import datetime, timedelta
 import requests  # Discordへの送信に使用
 import os
 import time
-from discord_util import DiscordUtil as DiscordUtilClass
+from discord_util import DiscordUtil
+from arxiv import fetch_feed
 
-# リクエストの再試行回数と待機時間を設定
-MAX_RETRIES = 3
-RETRY_WAIT_TIME = 5  # 秒
-discord_util = DiscordUtilClass()  # インスタンス生成
-
-# フィードを取得する関数を定義
-def fetch_feed(url, retries=MAX_RETRIES):
-    for attempt in range(retries):
-        try:
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            }
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()  # HTTPエラーが発生した場合に例外を発生させる
-            return feedparser.parse(response.content)
-        except (requests.exceptions.RequestException, ConnectionResetError) as e:
-            print(f"Attempt {attempt + 1} failed: {e}")
-            if attempt < retries - 1:
-                time.sleep(RETRY_WAIT_TIME)
-            else:
-                print("Failed to fetch feed after multiple attempts.")
-                raise  # 最終試行でも失敗した場合は例外を再発生させる
+discord_util = DiscordUtil()  
 
 def run():
     # 現在のUTC時刻を取得
