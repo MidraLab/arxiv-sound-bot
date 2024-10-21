e04050415bb1db8042747dbc3843e10320e2d922
inamoto <inamoto_yuta@cyberagent.co.jp>
2024-10-21 21:07:28 +0900

    fix: 定期実行を12時間ごとに変更

--- source/fetch_arxiv_papers.py
+++ source/fetch_arxiv_papers.py
@@ -11,26 +11,25 @@ discord_util = DiscordUtil()
 JSON_FILE_DIR = "opt"
 JSON_FILE_PATH = os.path.join(JSON_FILE_DIR, "contents_info.json")
 
-def save_latest_id(latest_id):
-    """最新のIDを保存する"""
+def save_latest_entry(latest_entry):
+    """最新のエントリ（IDや公開日など）を保存する"""
     if not os.path.exists(JSON_FILE_DIR):
         os.makedirs(JSON_FILE_DIR)
     with open(JSON_FILE_PATH, 'w') as f:
-        json.dump({"latest_id": latest_id}, f)
+        json.dump(latest_entry, f)
 
-def load_latest_id():
-    """最新のIDを読み込む"""
+def load_latest_entry():
+    """最新のエントリ（IDや公開日など）を読み込む"""
     if os.path.exists(JSON_FILE_PATH):
         with open(JSON_FILE_PATH, 'r') as f:
             data = json.load(f)
-            return data.get("latest_id", None)
-    # ファイルが存在しない場合は空のファイルを作成する
+            return data
     else:
         if not os.path.exists(JSON_FILE_DIR):
             os.makedirs(JSON_FILE_DIR)
         with open(JSON_FILE_PATH, 'w') as f:
-            json.dump({"latest_id": ""}, f)
-    return None
+            json.dump({}, f)
+    return {}
 
 def parse_date(date_str):
     """日付を解析する"""
@@ -65,8 +64,9 @@ def run():
     # フィードを取得
     feed = fetch_feed(url)
 
-    # 最新のIDを読み込む
-    latest_id = load_latest_id()
+    # 最新のエントリ情報を読み込む
+    latest_entry = load_latest_entry()
+    latest_id = latest_entry.get("latest_id", None)
 
     # Discordに送信した論文の数をカウント
     paper_count = 0
@@ -74,7 +74,8 @@ def run():
     # 各論文について、最新のIDと比較して新しいものを処理する
     for entry in feed.entries:
         current_id = entry.id.split('/')[-1]
-        
+        current_date = entry.published
+
         # 最新のIDが存在しないか、現在のIDが最新のIDよりも新しい場合に処理を続行
         if latest_id is not None and current_id == latest_id:
             print("最新のコンテンツまで到達しました。")
@@ -83,9 +84,12 @@ def run():
         discord_util.send_message(entry)
         paper_count += 1
 
-        # 最初のエントリのIDを保存
+        # 最初のエントリのデータを保存
         if paper_count == 1:
-            save_latest_id(current_id)
+            save_latest_entry({
+                "latest_id": current_id,
+                "published_date": current_date
+            })
 
         time.sleep(5)  # 連続して送信しないように5秒待機
     
